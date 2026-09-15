f9733de37867cb595af40ab96be17b55e404c428
raclearn168-gh <raclearn9@gmail.com>
2026-09-16 00:11:17 +0800

    Datamodel yy (#7)
    * staging models
    intermediate models - business logic for computation of metrics and KPIs
    
    * business logic transformation for customer orders and orders data
    
    * 1. EDA jypter notebooks - 01_olist_eda.ipynb.ipynb, eda_finding.ipynb
    2. upated dbt_project.yml to change schema for marts to olist_mart
    3. updated generate_schema_name.sql macro to return target.schema if custom_schema_name is None
    4. Generate the dim_date model in marts folder
    
    * model for dimensional tables and fact tables for the data warehouse. The changes include modifications to existing staging models and the addition of new models for customer, geolocation, product, seller, order items, orders, payments, reviews
    
    * 1. Updated the models to fix the join types in the intermediate models and corrected the source references in the staging models.
    2. Cleaned up the fct_customer_rfm.sql to ensure proper calculation of recency_days and removed unnecessary comments.
    3. Cleaned up the data field name to match the ER diagram
    4. Updated the staging models to ensure proper source references and cleaned up the holiday calendar model to correctly reference the source tables.
    5. Include holiday calendar data for both 2017 and 2018 in the staging model to ensure comprehensive coverage of holiday data
    6. Include holiday information in dim_date model to provide context for order dates and customer behavior analysis.
    
    * Updated the changes highlighted in the PR review.
    
    * Updated to change the geolocation model to include additional fields
    for latitude and longitude, and to improve the accuracy of location data.
    This update will provide 1 row = 1 geolocation_zip_code_prefix
    
    * update to include retrieve the last order's geolocation information for each customer in the dim_customer model.
    This involves joining the last_orders_geolocation CTE to the customers table and selecting the relevant fields (customer_zip_code_prefix, customer_city, customer_state) from it.
    
    * added eda for rfm
    updated fct_customer_rfm model using percentile ranking and business logic
    
    * updated the dependencies
    
    * mart_models
    
    * updated mart model
    
    * updated the models
    
    * updated model
    
    * updated comments
    
    * fix indentation issue

---
+++ dbt_transform/models/intermediate/int_geolocation.sql
@@ -0,0 +1,19 @@
+/*
+ * It deduplicates the data based on zip code prefix, latitude, longitude, city, and state.
+ * The region is derived based on the state.
+ */
+
+{{ config(materialized='view') }}
+
+SELECT
+    CAST(geolocation_zip_code_prefix AS STRING) AS geolocation_zip_code_prefix,
+    latitude,
+    longitude,
+    city,
+    state
+FROM {{ ref('stg_geolocation') }}
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY geolocation_zip_code_prefix
+    ORDER BY city, state, latitude, longitude
+) = 1
+
